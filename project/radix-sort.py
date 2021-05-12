329b302ac7f836cbc2992543e703de17b02af7c8
Ian Sornson <pippyct55@gmail.com>
2021-05-11 19:41:34 -0500

    finished radix sort

--- project/radix-sort.py
+++ project/radix-sort.py
@@ -7,4 +7,42 @@ def book_to_words(book_url='https://www.gutenberg.org/files/84/84-0.txt'):
     return bookascii.split()
 
 def radix_a_book(book_url='https://www.gutenberg.org/files/84/84-0.txt'):
-    pass
+    words = book_to_words()
+    
+    longest = len(words[0])
+    for i in range(len(words)):
+        if len(words[i]) > longest:
+            longest = len(words[i])
+    
+    for i in range(len(words)):
+        words[i] += '\0'.encode('ascii', 'replace') * (longest - len(words[i]))
+
+    for i in range(longest - 1, -1, -1):
+        words = counting_sort(words, i)
+    
+    for i in range(len(words)):
+        words[i] = words[i].decode('ascii').replace('\x00', '')
+        words[i] = bytes(words[i], encoding='ascii')
+
+    # print(words)
+    return words
+    
+def counting_sort(lst, index):
+    count = [0] * 127
+    output = [0] * len(lst)
+
+    for word in lst:
+        count[word[index]] += 1
+
+    for i in range(1, len(count)):
+        count[i] += count[i - 1]
+    
+    for i in range(len(lst) - 1, -1, -1):
+        idx = lst[i][index]
+        output[count[idx] - 1] = lst[i]
+        count[idx] -= 1
+    
+    for i in range(0, len(lst)):
+        lst[i] = output[i]
+    
+    return lst
